9d6e6396de214fbcbf139f8df75888fe84737bc9
veronikapodgol <veronika.podgol@gmail.com>
2022-07-10 23:25:41 +0300

    Initial commit

---
+++ src/test/java/helpers/BrowserStack.java
@@ -0,0 +1,26 @@
+package helpers;
+
+import org.aeonbits.owner.ConfigFactory;
+import owner.BrowserStackConfig;
+
+import static io.restassured.RestAssured.given;
+import static java.lang.String.format;
+
+public class BrowserStack {
+    static BrowserStackConfig config = ConfigFactory.create(BrowserStackConfig.class, System.getProperties());
+
+    public static String videoUrl(String sessionId) {
+        String url = format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
+
+        return given()
+                .auth().basic(config.username(), config.accessKey())
+                .log().all()
+                .when()
+                .get(url)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .path("automation_session.video_url");
+    }
+}
